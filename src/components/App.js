8264750596125afe89baebeb0c8ec8e16f6f6ede
MietoProgramming <kamdox7@gmail.com>
2021-06-22 16:43:02 +0200

    Loading animation, on hover feature, added likes, comments and time statistics.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,10 @@ function App() {
   useEffect(() => {
 
     const fetchData = async () => {
+      const getTime = (start, end) => {
+        return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+      }
+
       let posts = await fetch("https://jsonplaceholder.typicode.com/posts").then(res => res.json());
       console.log(posts);
       let comments = await fetch("https://jsonplaceholder.typicode.com/comments").then(res => res.json());
@@ -22,8 +26,12 @@ function App() {
       let users = await fetch(`https://randomuser.me/api/?seed=${seedForAPI}&results=10`).then(res => res.json());
 
       let finalPosts = posts.map(post => {
-        return { ...post, comments: comments.filter(comment => comment.postId === post.id) }
+        const date = getTime(new Date(Date.now() - 10000000), new Date());
+        const likes = Math.floor(Math.random() * 100);
+        return { ...post, date, likes, comments: comments.filter(comment => comment.postId === post.id) }
       });
+
+
 
       dispatch(setupPosts(finalPosts.sort((a, b) => (Math.random() > 0.5 ? -1 : 1))));
       dispatch(setupTodos(todos));
@@ -38,7 +46,7 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <div className="main">{!loading &&
+      <div className="main">{loading ? <div className="loadingBox"></div> :
         <PostWall posts={posts} />}</div>
     </div>
   );
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,17 +1,33 @@
-import useState from "react";
+import { useState, useCallback } from "react";
+import { FaComment, FaHeart, FaHourglassHalf } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 export const Post = ({ post, setTodos, user }) => {
+  const [hover, setHover] = useState(false);
   const showComments = () => {};
 
   const showTodo = () => {
     setTodos(user);
   };
 
-  const onPostHover = (hover) => {};
+  const getTimeFromPublish = (time) => {
+    const difference = Date.now() - time.getTime();
+    if (difference / 1000 <= 60) return `${Math.floor(difference / 1000)}s`;
+    else if (difference / 1000 / 60 <= 60)
+      return `${Math.floor(difference / 60000)}m`;
+    else if (difference / 1000 / 3600 <= 24)
+      return `${Math.floor(difference / 1000 / 3600)}h`;
+    else return `${Math.floor(difference / 1000 / 3600 / 24)}d`;
+  };
 
   return (
     <div>
-      <div className="post" onClick={showComments()}>
+      <div
+        className="post"
+        onClick={showComments()}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
         <div className="top">
           <img
             src={user.picture.thumbnail}
@@ -27,19 +43,37 @@ export const Post = ({ post, setTodos, user }) => {
           <p>{post.body}</p>
         </div>
       </div>
-      <div className="onHoverPost" onMouseEnter={onPostHover(true)}>
+      <div
+        className={hover ? "onHoverPost active" : "onHoverPost"}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
         <div>
-          <img src="" alt="Likes" />
-          96
+          <IconContext.Provider
+            value={{ color: "red", size: 75, className: "icon" }}
+          >
+            <FaHeart />
+          </IconContext.Provider>
+          <br />
+          <span>{post.likes}</span>
         </div>
         <div>
-          <img src="" alt="Comments" />
-          13
+          <IconContext.Provider
+            value={{ color: "#4a96d9", size: 75, className: "icon" }}
+          >
+            <FaComment />
+          </IconContext.Provider>
+          <br />
+          {post.comments.length}
         </div>
         <div>
-          {" "}
-          <img src="" alt="Time since upload" />
-          2h{" "}
+          <IconContext.Provider
+            value={{ color: "grey", size: 75, className: "icon" }}
+          >
+            <FaHourglassHalf />
+          </IconContext.Provider>
+          <br />
+          {getTimeFromPublish(post.date)}
         </div>
       </div>
     </div>
